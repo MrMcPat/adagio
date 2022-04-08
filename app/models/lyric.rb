de14bb9978696b0112a39d0b09fd6ef46ee85f71
Patrick Liang <pl204511194@yahoo.com>
2022-04-07 22:51:54 -0400

    small push

--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -2,8 +2,4 @@ class Lyric < ApplicationRecord
     has_many :responses
     has_many :users, through: :responses
 
-    # def self.today
-    #     where(:created_at => (Time.zone.now.beginning_of_day..Time.zone.now)).all.any?
-    #   end
-
 end
