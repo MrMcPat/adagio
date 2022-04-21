98e64237d0404b4ecb48c552be1664018be51484
Patrick Liang <pl204511194@yahoo.com>
2022-04-21 10:52:35 -0400

    downgrade react version

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { BrowserRouter } from "react-router-dom"
 import './index.css'
 import App from './components/App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
