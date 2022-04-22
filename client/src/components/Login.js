712135a1ba1fd551a9f74c66580daa0611c099b2
Patrick Liang <pl204511194@yahoo.com>
2022-04-21 21:28:43 -0400

    deployment fix

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -17,7 +17,7 @@ function Login({setUser}) {
       }).then((r) => {
         if (r.ok) {
           r.json().then((user) => setUser(user))
-          window.location = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&response_type=token&scope=streaming user-read-email user-modify-playback-state user-read-private user-read-private user-read-playback-state&show_dialog=true&redirect_uri=http://localhost:4000/callback`
+          window.location = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&response_type=token&scope=streaming user-read-email user-modify-playback-state user-read-private user-read-private user-read-playback-state&show_dialog=true&redirect_uri=https://adagio-app.herokuapp.com/callback`
         } else {
           r.json().then(
             (err) => 
